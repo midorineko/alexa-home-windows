6c38697a9b3dedc3f2920cd8285db01ab32c26d4
Steven Inouye <steven.inouye91@gmail.com>
2015-04-03 19:24:12 -0700

    twitch now can play most popular -notifies me who is playing -allows user to chose who they view

--- modules/twitch.rb
+++ modules/twitch.rb
@@ -1,7 +1,9 @@
 def process_twitch(command, hue)
+  words = command.split(" ")
   ENV["SSL_CERT_FILE"] = "C:/Ruby21/lib/ruby/2.1.0/rubygems/ssl_certs/cacert.pem"
   dota = JSON.parse open('https://api.twitch.tv/kraken/streams?limit=10&game=Dota%202').read
   active_streams = dota['streams']
+  light1, light2 = hue.lights
 
   streamers = []
   viewer_count = []
@@ -13,6 +15,9 @@ def process_twitch(command, hue)
     url << x['channel']['url']
     status << x['channel']['status']
   end
+  url = url.reverse
+  status = status.reverse
+  viewer_count = viewer_count.reverse
   p streamers
   p viewer_count
   p url
@@ -27,34 +32,102 @@ def process_twitch(command, hue)
 
   need_to_watch = {}
 
-  streamers.each_with_index do |name, i|
+def live(life, light2)
+  if life === false
+    light2.red
+    light2.blip
+  else
+    light2.green
+    light2.blip
+  end
+end
+
+  streamers.reverse.each_with_index do |name, i|
     if name == 'Sing_sing'
+      light1.xy = [0.3739, 0.2445]
+      light2.xy = [0.3739, 0.2445]
+      light2.blip
+      light1.blip
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i]]
     elsif name == 'Sheever'
       need_to_watch[name] = [viewer_count[i], url[i]]
     elsif name == 'MerliniDota'
+      light1.xy = [0.2657, 0.4364]
+      light2.xy = [0.2657, 0.4364]
+      light2.blip
+      light1.blip
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i]]
     elsif name == 'Pyrionflax'
+      light1.xy = [0.3739, 0.2445]
+      light2.xy = [0.3739, 0.2445]
+      light2.blip
+      light1.blip
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i]]
     elsif name == 'EternaLEnVyy'
+      light1.xy = [0.3739, 0.2445]
+      light2.xy = [0.3739, 0.2445]
+      light2.blip
+      light1.blip
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i]]
-    elsif name ==  'wagamamaTv'
+    elsif name ==  'WagamamaTV'
+      light1.xy  = [0.4951, 0.4807]
+      light2.xy  = [0.4951, 0.4807]
+      light2.blip
+      light1.blip
+      sleep 2
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name ==  'BananaSlamJamma'
+      light1.xy  = [0.6413, 0.3513]
+      light2.xy  = [0.6413, 0.3513]
+      light2.blip
+      light1.blip
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i]]
     elsif name ==  'BeyondTheSummit'
+      light1.xy  = [0.2162, 0.108]
+      light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
+      live(live, light2)
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i], live]
     elsif name == 'dotastarladder_en'
+      light1.xy  = [0.2127, 0.1076]
+      light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
+      live(live, light2)
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i], live]
     elsif name == 'joindotared'
+      light1.xy = [0.525, 0.2228]
+      light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
+      live(live, light2)
+      sleep 2
       need_to_watch[name] = [viewer_count[i], url[i], live]
     end
   end
 
   def watch_top_game(games)
-    Launchy.open(games.first[1][1])
+    hero = games.keys.last
+    Launchy.open(games[hero][1])
   end
-  watch_top_game(need_to_watch)
+
+  def watch_specific_game(games, num)
+    hero = games.keys[num]
+    Launchy.open(games[hero][1])
+  end
+
+  if command.scan(/watch/).length > 0
+    watch_top_game(need_to_watch)
+  end
+
+  if command.scan(/stream/).length > 0
+    which_stream = words.pop.in_numbers
+    watch_specific_game(need_to_watch, which_stream)
+  end
 
 end
