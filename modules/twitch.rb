f678fe4a5da9b04d4f64a67659fccac025fe50fd
Steven Inouye <steven.inouye91@gmail.com>
2015-04-03 18:23:51 -0700

    hard days work

--- modules/twitch.rb
+++ modules/twitch.rb
@@ -3,18 +3,58 @@ def process_twitch(command, hue)
   dota = JSON.parse open('https://api.twitch.tv/kraken/streams?limit=10&game=Dota%202').read
   active_streams = dota['streams']
 
-  p '============================================================'
   streamers = []
   viewer_count = []
   url = []
+  status = []
   active_streams.each do |x|
     streamers << x['channel']['display_name']
     viewer_count << x['viewers']
     url << x['channel']['url']
+    status << x['channel']['status']
   end
   p streamers
   p viewer_count
   p url
-  p '============================================================'
+
+  if viewer_count .first > 15000
+    p 'over 15000'
+  elsif viewer_count .first > 10000
+    p 'over 10000'
+  else
+    p 'less than 10000'
+  end
+
+  need_to_watch = {}
+
+  streamers.each_with_index do |name, i|
+    if name == 'Sing_sing'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name == 'Sheever'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name == 'MerliniDota'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name == 'Pyrionflax'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name == 'EternaLEnVyy'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name ==  'wagamamaTv'
+      need_to_watch[name] = [viewer_count[i], url[i]]
+    elsif name ==  'BeyondTheSummit'
+      live = status[i]["REBROADCAST"] ?  false : true
+      need_to_watch[name] = [viewer_count[i], url[i], live]
+    elsif name == 'dotastarladder_en'
+      live = status[i]["REBROADCAST"] ?  false : true
+      need_to_watch[name] = [viewer_count[i], url[i], live]
+    elsif name == 'joindotared'
+      live = status[i]["REBROADCAST"] ?  false : true
+      need_to_watch[name] = [viewer_count[i], url[i], live]
+    end
+  end
+
+  def watch_top_game(games)
+    Launchy.open(games.first[1][1])
+  end
+  watch_top_game(need_to_watch)
 
 end
