240d973045a4bfd658526e50f5041be5d93b66f5
Steven Inouye <steven.inouye91@gmail.com>
2015-04-03 19:51:41 -0700

    added twitch to stream and keep colors post change

--- modules/twitch.rb
+++ modules/twitch.rb
@@ -18,9 +18,6 @@ def process_twitch(command, hue)
   url = url.reverse
   status = status.reverse
   viewer_count = viewer_count.reverse
-  p streamers
-  p viewer_count
-  p url
 
   if viewer_count .first > 15000
     p 'over 15000'
@@ -34,12 +31,13 @@ def process_twitch(command, hue)
 
 def live(life, light2)
   if life === false
-    light2.red
-    light2.blip
+    lit = light2.red
   else
-    light2.green
-    light2.blip
+    lit = light2.green
   end
+  lit
+  light2.blip
+  return lit
 end
 
   streamers.reverse.each_with_index do |name, i|
@@ -49,85 +47,78 @@ end
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
-    elsif name == 'Sheever'
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.3739, 0.2445], [0.3739, 0.2445], viewer_count[i], url[i]]
     elsif name == 'MerliniDota'
       light1.xy = [0.2657, 0.4364]
       light2.xy = [0.2657, 0.4364]
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.2657, 0.4364], [0.2657, 0.4364], viewer_count[i], url[i]]
     elsif name == 'Pyrionflax'
       light1.xy = [0.3739, 0.2445]
       light2.xy = [0.3739, 0.2445]
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.3739, 0.2445], [0.3739, 0.2445], viewer_count[i], url[i]]
     elsif name == 'EternaLEnVyy'
       light1.xy = [0.3739, 0.2445]
       light2.xy = [0.3739, 0.2445]
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.3739, 0.2445], [0.3739, 0.2445], viewer_count[i], url[i]]
     elsif name ==  'WagamamaTV'
       light1.xy  = [0.4951, 0.4807]
       light2.xy  = [0.4951, 0.4807]
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.4951, 0.4807],[0.4951, 0.4807],viewer_count[i], url[i]]
     elsif name ==  'BananaSlamJamma'
       light1.xy  = [0.6413, 0.3513]
       light2.xy  = [0.6413, 0.3513]
       light2.blip
       light1.blip
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i]]
+      need_to_watch[name] = [[0.6413, 0.3513], [0.6413, 0.3513], viewer_count[i], url[i]]
     elsif name ==  'BeyondTheSummit'
       light1.xy  = [0.2162, 0.108]
       light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
       live(live, light2)
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i], live]
+      need_to_watch[name] = [[0.2162, 0.108], live(live, light2), viewer_count[i], url[i], live]
     elsif name == 'dotastarladder_en'
       light1.xy  = [0.2127, 0.1076]
       light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
       live(live, light2)
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i], live]
+      need_to_watch[name] = [[0.2127, 0.1076], live(live, light2), viewer_count[i], url[i], live]
     elsif name == 'joindotared'
       light1.xy = [0.525, 0.2228]
       light1.blip
       live = status[i]["REBROADCAST"] ?  false : true
       live(live, light2)
       sleep 2
-      need_to_watch[name] = [viewer_count[i], url[i], live]
+      need_to_watch[name] = [[0.525, 0.2228], live(live, light2), viewer_count[i], url[i], live]
     end
   end
 
-  def watch_top_game(games)
-    hero = games.keys.last
-    Launchy.open(games[hero][1])
+  def watch_specific_game(games, light1, light2, num)
+    num == 0 ? num = games.keys.length - 1 : num = num
+    hero = games.keys[num]
+    light1.xy = games[hero][0]
+    light2.xy = games[hero][1]
+    Launchy.open(games[hero][3])
   end
 
-  def watch_specific_game(games, num)
-    hero = games.keys[num]
-    Launchy.open(games[hero][1])
-  end
 
   if command.scan(/watch/).length > 0
-    watch_top_game(need_to_watch)
-  end
-
-  if command.scan(/stream/).length > 0
     which_stream = words.pop.in_numbers
-    watch_specific_game(need_to_watch, which_stream)
+    watch_specific_game(need_to_watch, light1, light2, which_stream)
   end
 
 end
